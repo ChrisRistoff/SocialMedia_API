c65b8b9dcd2e40c5171dd19301a348a300035ef0
Krasyo <doanusho@gmail.com>
2023-11-11 22:41:41 +0000

    add small fixes to db schema

--- db/forum_schema.sql
+++ db/forum_schema.sql
@@ -1,10 +1,8 @@
+CREATE DATABASE forum_db;
+
 \c forum_db
 
-CREATE TABLE IF NOT EXISTS js_be (
-    threads VARCHAR(100)
-);
-
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     username TEXT,
@@ -12,36 +10,40 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT
 );
 
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE IF NOT EXISTS categories(
     category_id SERIAL PRIMARY KEY,
     category_name TEXT,
     description TEXT
 );
+
+INSERT INTO categories (category_name, description) VALUES
+    ( 'JavaScript BE', 'Everything JS backend' ),
+    ( 'DATABASES', 'Everything DATABASES' );
 
 CREATE TABLE IF NOT EXISTS threads(
     thread_id SERIAL PRIMARY KEY,
     content TEXT,
     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     category_id INTEGER REFERENCES categories(category_id),
-    user_id FOREIGN KEY REFERENCES users(user_id)
+    user_id INTEGER REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS posts(
     post_id SERIAL PRIMARY KEY,
     post_content TEXT,
     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id FOREIGN KEY REFERENCES users(user_id),
-    thread_id FOREIGN KEY REFERENCES threads(thread_id)
-)
-
-CREATE TABLE IF NOT EXISTS reactions (
-    reaction_id SERIAL PRIMARY KEY,
-    reaction_type VARCHAR(20),
-    post_id FOREIGN KEY REFERENCES posts(post_id),
-    user_id FOREIGN KEY REFERENCES users(user_id)
+    user_id INTEGER REFERENCES users(user_id),
+    thread_id INTEGER REFERENCES threads(thread_id)
 );
 
-CREATE TABLE IF NOT EXISTS roles (
-    user_id FOREIGN KEY REFERENCES users(user_id),
-    role VARCHAR(20)
+CREATE TABLE IF NOT EXISTS reactions(
+    reaction_id SERIAL PRIMARY KEY,
+    reaction_type VARCHAR(20),
+    post_id INTEGER REFERENCES posts(post_id),
+    user_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS roles(
+    role VARCHAR(20),
+    user_id INTEGER REFERENCES users(user_id)
 );
