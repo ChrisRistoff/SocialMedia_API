59e76ed46f34cb1c234b472fb7e9817e51aae903
Krasyo <doanusho@gmail.com>
2023-11-12 14:52:12 +0000

    add tests for reply route

--- __tests__/posts.test.ts
+++ __tests__/posts.test.ts
@@ -13,8 +13,8 @@ describe('create post', () => {
     const auth = await supertest(app)
       .post("/signin")
       .send({
-        email: "krsnhrstv@gmail.com",
-        password: "krasen"
+        email: "test@test.test",
+        password: "test"
       })
 
     token = auth.body.token
@@ -31,7 +31,7 @@ describe('create post', () => {
       const res = await supertest(app)
         .post("/posts")
         .send({
-          thread_id: 26,
+          thread_id: 1,
           user_id: 1,
           content: "test post content"
         })
@@ -45,14 +45,14 @@ describe('create post', () => {
         .post("/posts")
         .set("Authorization", `Bearer ${token}`)
         .send({
-          thread_id: 26,
+          thread_id: 1,
           user_id: 1,
           post_content: "test post content"
         })
 
       const post = res.body.post
       expect(res.statusCode).toBe(201)
-      expect(post.user.username).toBe("krasen")
+      expect(post.user.username).toBe("test")
       expect(post.post_content).toBe("test post content")
     })
   })
@@ -62,7 +62,7 @@ describe('create post', () => {
       .post("/posts")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        thread_id: 26,
+        thread_id: 1,
         user_id: 1,
         post_content: ""
       })
@@ -89,13 +89,103 @@ describe('create post', () => {
       .post("/posts")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        thread_id: 26,
+        thread_id: 1,
         user_id: 145,
         post_content: "test post content"
       })
 
     expect(res2.statusCode).toBe(500)
     expect(res2.body.error).toBe("Internal server error")
+  })
+
+  describe('replies', () => {
+    it('should return an error when the user is not signed in', async () => {
+      const res = await supertest(app)
+        .post("/posts")
+        .send({
+          thread_id: 1,
+          user_id: 1,
+          post_content: "test post content"
+      })
+
+      expect(res.statusCode).toBe(401)
+      expect(res.body.error).toBe("You need to be logged in")
+    })
+
+    it('should create a new reply when all of the parameters are given', async () => {
+      const res = await supertest(app)
+        .post("/replies")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          thread_id: 1,
+          user_id: 1,
+          post_content: "test reply content",
+          reply_to_post_id: 1
+      })
+
+      expect(res.statusCode).toBe(201)
+      const reply = res.body.reply
+      expect(reply.user.username).toBe("test")
+      expect(reply.post_content).toBe("test reply content")
+      expect(reply.post.post_id).toBe(1)
+    })
+
+    it('should throw an error when content is empty', async () => {
+       const res = await supertest(app)
+        .post("/replies")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          thread_id: 1,
+          user_id: 1,
+          post_content: "",
+          reply_to_post_id: 1
+      })
+
+      expect(res.statusCode).toBe(400)
+      expect(res.body.error).toBe("Missing parameters")
+
+    })
+
+    it('should throw an error when wrong IDs are given', async () => {
+      const res = await supertest(app)
+        .post("/replies")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          thread_id: 1,
+          user_id: 1,
+          post_content: "test reply content",
+          reply_to_post_id: 50000
+      })
+
+      expect(res.statusCode).toBe(500)
+      expect(res.body.error).toBe("Internal server error")
+
+      const res2 = await supertest(app)
+        .post("/replies")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          thread_id: 1,
+          user_id: 5000,
+          post_content: "test reply content",
+          reply_to_post_id: 1
+      })
+
+      expect(res2.statusCode).toBe(500)
+      expect(res2.body.error).toBe("Internal server error")
+
+      const res3 = await supertest(app)
+        .post("/replies")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          thread_id: 5000,
+          user_id: 1,
+          post_content: "test reply content",
+          reply_to_post_id: 1
+      })
+
+      expect(res3.statusCode).toBe(500)
+      expect(res3.body.error).toBe("Internal server error")
+    })
   })
 
 })
