6f68f89db0a930cf796c67ff5266f68731fb0ec2
Krasyo <doanusho@gmail.com>
2023-11-14 10:34:26 +0000

    removing the next() function solved the issue in my previous push

--- middleware/validationError.ts
+++ middleware/validationError.ts
@@ -1,12 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-export const handleInputError = (req: Request, res: Response, next: NextFunction) => {
+export const handleInputError = (req: Request, res: Response) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
+    console.log(errors.array())
     res.status(400).send({ errors: errors.array() })
   }
-
-  next()
 }
