fbe2bf6e62e6c4196b68472fe025d146f1a49ebd
Krasyo <doanusho@gmail.com>
2023-11-14 10:29:22 +0000

    add basic input validators and middleware to catch them, currently they crash the app, will have a look at it after class

--- routers/postRoutes.ts
+++ routers/postRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware";
 import { createPost, getAllPosts, replyToPost } from "../controllers/postsController";
+import { body } from "express-validator";
+import { handleInputError } from "../middleware/validationError";
+
 export const postsRouter = Router()
 export const protectedPostsRouter = Router()
 
-protectedPostsRouter.post("/posts", protect, createPost)
-protectedPostsRouter.post("/replies", protect, replyToPost)
+protectedPostsRouter.post("/posts",
+  protect,
+  body("post_content").isString().notEmpty().isLength({min: 100}),
+  handleInputError,
+  createPost)
+
+protectedPostsRouter.post("/replies",
+  protect,
+  body("post_content").isString().notEmpty().isLength({min: 100}),
+  handleInputError,
+  replyToPost)
 
 postsRouter.get("/posts", getAllPosts)
--- routers/threadRouters.ts
+++ routers/threadRouters.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { createThread } from "../controllers/threadControllers";
 import { protect } from "../middleware/authMiddleware";
+import { body } from "express-validator";
 
 export const threadsRouterProtected = Router()
 export const threadsRouter = Router()
 
-threadsRouterProtected.post("/threads", protect, createThread)
+threadsRouterProtected.post("/threads",
+  body("title").isString().isLength({min: 100}),
+  body("description").isString().isLength({min: 100}),
+  protect,
+  createThread)
